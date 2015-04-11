9eddfa7466f1408c321ed2bf699794c9bb5c1997
Swapnal <acharya.swapnal@gmail.com>
2015-04-11 12:23:54 +0530

    Added User Role:Admin,Manager,Teammember and modified charts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
-  
+  enum role: [:manager, :admin, :teammember]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :teammember
+  end
   has_many :time_entries ,dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
