59fff4b54f7734d041b028505c926b87e83dd478
Harsha Chaudhary <harsha.chaudhary@gmail.com>
2015-04-11 13:40:35 +0530

    Harsha - Adding start date to project

--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -2,5 +2,4 @@ class TimeEntry < ActiveRecord::Base
   belongs_to :project
   belongs_to :task
   belongs_to :user
-  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150411071815) do
+ActiveRecord::Schema.define(version: 20150411080602) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date     "startDate"
   end
 
   create_table "tasks", force: :cascade do |t|
